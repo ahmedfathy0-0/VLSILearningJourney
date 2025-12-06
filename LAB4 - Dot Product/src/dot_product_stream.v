212264c412c69aa4881b51fc4f6b91b0dcd7b165
ZiadMontaser <ziadmontaser2005@gmail.com>
2025-12-06 19:15:35 +0200

    Implemented dot_product_stream

--- LAB4 - Dot Product/src/dot_product_stream.v
+++ LAB4 - Dot Product/src/dot_product_stream.v
@@ -7,13 +7,12 @@ module dot_product #(
     
     // --- Handshake Interface ---
     input  wire input_valid,
-    output reg  input_ready,
     
     // --- Data Inputs ---
     // Flattened Vectors: [ (WIDTH*N)-1 : 0 ]
     // Example: For 4x8-bit, this is [31:0]
-    input  wire [(WIDTH*N)-1:0] A_vec,
-    input  wire [(WIDTH*N)-1:0] B_vec,
+    input  wire [WIDTH-1:0] A_vec,
+    input  wire [WIDTH-1:0] B_vec,
     
     // --- Output ---
     // Accumulator Size = (2 * WIDTH) + log2(N) to prevent overflow
@@ -22,79 +21,50 @@ module dot_product #(
 );
 
     // --- Internal Storage ---
-    reg [(WIDTH*N)-1:0] A_reg;
-    reg [(WIDTH*N)-1:0] B_reg;
+    reg [WIDTH-1:0] A_reg;
+    reg [WIDTH-1:0] B_reg;
+
+    reg [(2*WIDTH + $clog2(N)) - 1 : 0] multiply_result;
+    reg last_input;
+    reg last_input_mul_add;
     
     // --- Control Logic ---
     // Counter needs enough bits to count up to N
     reg [$clog2(N)-1:0] counter;
-    reg [1:0] state;
-    
-    localparam STATE_IDLE    = 2'b00;
-    localparam STATE_COMPUTE = 2'b01;
-    localparam STATE_DONE    = 2'b10;
-
-    // --- Dynamic Mux Logic (The "Cloud") ---
-    // These wires hold the single element selected by the counter
-    wire signed [WIDTH-1:0] a_element;
-    wire signed [WIDTH-1:0] b_element;
-
-    // "Indexed Part Select" Syntax: [START +: BLOCK_SIZE]
-    // If counter is 0, grabs [0 +: 8] -> bits [7:0]
-    // If counter is 1, grabs [8 +: 8] -> bits [15:8]
-    assign a_element = A_reg[counter * WIDTH +: WIDTH];
-    assign b_element = B_reg[counter * WIDTH +: WIDTH];
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            state        <= STATE_IDLE;
-            input_ready  <= 1'b0;
             output_valid <= 1'b0;
             result  <= 0;
+            multiply_result  <= 0;
             counter      <= 0;
             A_reg <= 0; B_reg <= 0;
+
+            last_input <= 0;
+            last_input_mul_add <= 0;
         end else begin
-            case (state)
-                STATE_IDLE: begin
-                    output_valid <= 1'b0;
-                    input_ready  <= 1'b1;
-                    
-                    if (input_valid && input_ready) begin
-                        A_reg       <= A_vec;
-                        B_reg       <= B_vec;
-                        input_ready <= 1'b0; // Lock inputs
-                        result <= 0;    // Clear accumulator
-                        counter     <= 0;
-                        state       <= STATE_COMPUTE;
-                    end
-                end
 
-                STATE_COMPUTE: begin
-                    // 1. Multiply the selected elements and Add to total
-                    // Note: This infers 1 Multiplier and 1 Adder reused N times
-                    result <= result + (a_element * b_element);
-                    
-                    // 2. Loop Control
-                    // Casting to clog2(N) bits to prevent width mismatch warnings
-                    if (counter == $clog2(N)'(N - 1)) begin 
-                        state <= STATE_DONE;
-                    end else begin
-                        counter <= counter + 1;
-                    end
-                end
+            if (input_valid) begin
+                A_reg <= A_vec;
+                B_reg <= B_vec;
 
-                STATE_DONE: begin
-                    output_valid <= 1'b1;
-                    state <= STATE_IDLE;
-                end
+                if(counter == N - 1) begin
+                    counter <= 0;
+                    last_input <= 1;
+                end else
+                    counter <= counter + 1;
 
-                default: begin
-                    // Fault Recovery: Go back to start
-                    state        <= STATE_IDLE;
-                    output_valid <= 1'b0;
-                    input_ready  <= 1'b0; // Safety: Don't accept input while recovering
-                end
-            endcase
+            end else begin
+                A_reg <= 0;
+                B_reg <= 0;
+                last_input <= 0;
+            end
+
+            multiply_result <= A_reg * B_reg;
+            last_input_mul_add <= last_input;
+
+            result <= result + multiply_result;
+            output_valid <= last_input_mul_add;
         end
     end
 
